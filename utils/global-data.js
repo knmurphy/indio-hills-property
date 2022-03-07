2a9cc7bf91f1d528b185f34cf85f466a391c1b01
Kevin N. Murphy <k.n.murphy@gmail.com>
2022-03-07 12:51:20 -0800

    testing embed and global variables Signed-off-by: Kevin N. Murphy <knmurphy@users.noreply.github.com>

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Indio Hills Land Lots';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Indio Hills Land Lots';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Contact Jay Scarcella';
 
   return {
     name,
